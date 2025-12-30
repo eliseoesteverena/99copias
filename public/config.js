05860e985153aea89ecb7f1a76951b8b12ac5bfe
eliseoesteverena <eliseo050595@gmail.com>
2025-12-30 20:52:31 -0300

    update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 // config.js - Configuración de Supabase
 const SUPABASE_CONFIG = {
   url: 'https://vkthaoiurwuzuztpvuyl.supabase.co', // Ej: https://xxxxx.supabase.co
-  anonKey: 'sb_publishable_B3sNAnwrbsAZHZeCTQhzjw_kONnukgc' // La clave pública larga
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrdGhhb2l1cnd1enV6dHB2dXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwODM3MDksImV4cCI6MjA4MjY1OTcwOX0.GfQWCwW4I_PeHijgafPhBmBAlS2lqT8aw9UaIY8TdXY' // La clave pública larga
 };
 
 // Inicializar cliente Supabase
